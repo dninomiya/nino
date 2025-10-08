ec68f8d0114f28dda2b8d0e5415ab282a663d183
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2025-10-08 23:20:49 +0900

    u

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 ```bash
 touch packages/db/local/local.db
+cp packages/db/.env.example packages/db/.env
+cp packages/auth/.env.example packages/auth/.env
 ```
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/libsql/web";
 
 export const db = drizzle({
   connection: {
-    url: process.env.TURSO_CONNECTION_URL!,
+    url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
   },
   schema: {},
