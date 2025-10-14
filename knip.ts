24098b4b6d0b853186978cbd07e6d285ef59b788
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2025-10-14 16:32:08 +0900

    Update knip configuration to expand ignored paths and add ignored dependencies. Remove obsolete @radix-ui/react-label from package.json and clean up pnpm-lock.yaml, improving project organization and dependency management.

--- knip.ts
+++ knip.ts
@@ -2,14 +2,36 @@ import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
   ignore: [
-    "apps/web/app/**/registry/**/*.mdx",
-    "apps/web/app/**/registry/**/*.tsx",
+    "apps/web/app/**/registry/**/*",
+    "apps/web/app/**/docs/**/*",
     "**/*/eslint.config.js",
-    "**/*/.eslintrc.js",
+    "**/.eslintrc.js",
     "**/*/global.ts",
     "**/*/i18n/request.ts",
     "apps/web/i18n/navigation.tsx",
   ],
+  ignoreDependencies: [
+    "rehype-slug",
+    "remark-code-import",
+    "remark-frontmatter",
+    "remark-gfm",
+    "remark-mdx-frontmatter",
+    "remark-reading-time",
+    "@tailwindcss/postcss",
+    "next",
+    "postcss",
+    "@eslint/js",
+    "postcss-load-config",
+    "@workspace/eslint-config",
+    "@typescript-eslint/eslint-plugin",
+    "@typescript-eslint/parser",
+    "eslint",
+    "eslint-config-next",
+    "tailwindcss",
+    "@tailwindcss/typography",
+    "@turbo/gen",
+  ],
+  ignoreBinaries: ["migrate", "turso", "eslint"],
 };
 
 export default config;
