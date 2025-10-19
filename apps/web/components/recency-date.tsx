a539e6e0f32d9f7384031fc3e516626f259df5b1
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2025-10-20 07:01:34 +0900

    up

--- apps/web/components/recency-date.tsx
+++ apps/web/components/recency-date.tsx
@@ -1,7 +1,18 @@
 "use client";
 
 import { formatDateByRecency } from "@/lib/util";
+import { useEffect, useState } from "react";
 
 export const RecencyDate = ({ date }: { date: Date | number }) => {
+  const [client, setClient] = useState(false);
+
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
+  if (!client) {
+    return null;
+  }
+
   return <span>{formatDateByRecency(date)}</span>;
 };
