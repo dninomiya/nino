1c08fd303e5ee8d617de4ba319a7fc70d11e06e7
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2025-10-28 15:10:51 +0900

    up

--- apps/web/app/[locale]/(main)/page.tsx
+++ apps/web/app/[locale]/(main)/page.tsx
@@ -27,7 +27,7 @@ export default async function Page({ params }: PageProps<"/[locale]">) {
     <>
       <div className="xl:flex gap-10 items-start px-4 xl:px-8">
         <div className="sticky top-header hidden xl:block">
-          <div className="w-80 overflow-y-auto h-[calc(100dvh-theme(spacing.header))] py-10">
+          <div className="w-80 overflow-y-auto h-[calc(100dvh-(var(--spacing-header)))] py-10">
             <h2 className="mb-6">{t.filterTitle}</h2>
             <Suspense>
               <FeedFilter feedItems={feedItems} />
